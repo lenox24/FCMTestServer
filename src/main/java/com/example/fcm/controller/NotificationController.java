ed185b61372cbce4b84cfb7d22b23d65a6b2ebd9
lenox24 <yk03171@naver.com>
2020-01-14 14:07:17 +0900

    =rewrite

--- src/main/java/com/example/fcm/controller/NotificationController.java
+++ src/main/java/com/example/fcm/controller/NotificationController.java
@@ -59,7 +59,6 @@ public class NotificationController {
     }
 
     @PostMapping("/token")
-
     public ResponseEntity token(@RequestBody JSONObject reqJson) {
         Token model = new Token(reqJson.get("token").toString());
         tokenRepo.insert(model);
